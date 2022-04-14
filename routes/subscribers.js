79b7a65e9d1b44d50b82960069d00d4f5beb9aff
pushpendra2621 <83192229+pushpendra2621@users.noreply.github.com>
2022-04-14 17:29:37 +0530

    Update delete function added

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -2,6 +2,7 @@ const express =  require('express')
 const router = express.Router()
 const Subscriber = require('../models/subscriber')
 
+//To get all the subscriber
 router.get('/', async (req, res)=>{
     try {
         const subscribers = await Subscriber.find()
@@ -13,6 +14,42 @@ router.get('/', async (req, res)=>{
    
 })
 
+// To get the subscriber from the id
+router.get('/:id', getSubscriber, (req, res)=>{
+   res.json(res.subscriber)   
+})
+
+
+//To delete the subscriber
+router.delete('/:id', getSubscriber, async(req, res)=>{
+    try{
+        await res.subscriber.deleteOne()
+        res.json({message:"The Subscriber deleted Successfully"})
+    } catch(error){
+        res.status(500).json({message:error.message})
+    }
+ })
+
+
+
+ //To update the subscriber
+ router.patch('/:id', getSubscriber, async (req, res)=>{
+     if(req.body.name!=null){
+         res.subscriber.name = req.body.name
+     }
+     if(req.body.subscribedToChannel!=null){
+        res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+    }
+    try{
+        const updatedSubscriber = await res.subscriber.save()
+        res.json(updatedSubscriber)
+    }catch(error){
+        res.status(400).json({message:error.message})
+    }
+ })
+
+
+ //To create the subscriber
 router.post('/', async (req, res)=>{
     const subscriber = new Subscriber({
         name: req.body.name,
@@ -26,6 +63,23 @@ router.post('/', async (req, res)=>{
     }
    
 })
+
+
+//Middleware function--Still dont know what it is just copy pasted the code.
+async function getSubscriber(req, res, next){
+    let subscriber
+    try{
+        subscriber = await Subscriber.findById(req.params.id)
+        if (subscriber==null){
+            return res.status(404).json({message:"Cannot Find the Subscriber"})
+        }
+
+    }catch(error){
+        return res.status(500).json({message:error.message})
+    }
+    res.subscriber = subscriber
+    next()
+}
 
 
 module.exports = router
